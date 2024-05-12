70c950dc480a8ad5051d20b3d3734aba67eeaf77
mahdieh1992 <mohamadimahdieh70@gmail.com>
2024-05-12 22:17:25 +0330

    create timer with component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// for write js in html we use of {}
-const timer = () => {
-  const elem = (
-    <div>
+// // for write js in html we use of {}
+// const timer = () => {
+//   const elem = (
+//     <div>
+//       <h2>This is timer</h2>
+//       {new Date().toLocaleTimeString()}
+//     </div>
+//   )
+//   ReactDOM.render(elem, document.getElementById('root'))// show time for 1 second in div root
+// }
+
+// // each 1 second refresh time for show current time 
+// setInterval(() => {
+//   timer()
+// }, 1000);
+
+
+// create component for timer
+
+
+
+class Describe extends React.Component {
+  render() {
+    return (
       <h2>This is timer</h2>
-      {new Date().toLocaleTimeString()}
-    </div>
-  )
-  ReactDOM.render(elem, document.getElementById('root'))// show time for 1 second in div root
+    )
+  }
 }
 
-// each 1 second refresh time for show current time 
-setInterval(() => {
-  timer()
-}, 1000);
 
+class Timer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Describe />
+        {new Date().toLocaleTimeString()}
+
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<Timer />, document.getElementById('root'))
